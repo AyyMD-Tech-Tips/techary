466c162787c3871a01d3c9e9fa1acd0aaee88792
TechXTT <71842793+TechXTT@users.noreply.github.com>
2020-10-04 11:51:41 +0300

    [ADD] Back-end

---
+++ debug for kalat/bootstrap-icons-1.0.0/post_try.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<html>
+    <head>
+        <style>
+            .post{
+                border: 1px solid #343536;
+                border-radius: 10px;
+                background-color: #1a1a1b;
+                text-align: center;
+                position: absolute;
+                color: #d2d5d7;
+
+            }
+        </style>
+    </head>
+    <body>
+        <div class="post">
+            <p>My ass BIIGIGI</p>
+        </div>
+    </body>
+</html>
